bd0f52f59696e177327fda63262d653f07b0682b
王嘉宁 <wangjianing@imdada.cn>
2020-04-21 19:30:25 +0800

    initial commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by 王嘉宁 on 2020/4/21.
+//  Copyright © 2020 Johnny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
